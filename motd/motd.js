c07668a9450c9c088364107985a10e2f3495e1a8
marc j. greenberg <marc@buildinglink.com>
2024-07-22 09:53:20 -0400

    containerize for testing

---
+++ motd/motd.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import prog from "caporal"
+import cqs from "./src/cqs.js"
+import { server } from "./src/app.js"
+import packageJson from "./package.json" assert { type: "json" }
+const { name, version, description } = packageJson
+
+try {
+   // =======================================================================
+   // cli processing
+   // =======================================================================
+   prog
+      .bin(name)
+      .version(version)
+      .description(description)
+
+      // =====================================================================
+      .command("refresh", "updates all cached data")
+      .option("-f, --force", "force a cache update", false)
+      .action(async (args, options) => {
+         try {
+            let cq = new cqs();
+            cq.refreshQuotes(options.force);
+         } catch (e) {
+            throw e;
+         }
+      })
+
+      // =====================================================================
+      .command("server", "start the service")
+      .action(async (args, options) => {
+         server();
+      });
+
+   if (process.argv.length < 3) process.argv.push("server")
+   prog.parse(process.argv)
+} catch (err) {
+   console.error(err)
+}
---
+++ motd/src/app.js
@@ -0,0 +1,31 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import qqs  from "./qqs.js"
+
+export function server() {
+   const app = express()
+   const PORT = process.env.GSPORT ?? 3000
+
+   app.use(bodyParser.json())
+   app.use(cors())
+
+   // Instantiate the query service
+   const qq = new qqs()
+
+   // Read all items using CQRS pattern
+   app.get("/v1/motd/quotes", async (req, res) => {
+      const returnMeta = req.query.hasOwnProperty("meta")
+      const result = (returnMeta) ? await qq.getQuotesMeta() : await qq.getAllQuotes()
+      res.status(result.status).send(result.data)
+   })
+
+
+   // Start the server
+   app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+      console.log(`Try http://localhost:${PORT}/v1/motd/quotes`)
+      console.log(`or  http://localhost:${PORT}/v1/motd/quotes?meta`)
+      console.log(`Press CTRL+C to stop`)
+   })
+}
